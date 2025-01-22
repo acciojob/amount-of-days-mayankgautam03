311902ce8db0e212939b8d985e77813be5f39cfe
Mayank Kumar Gautam <mayank.guts30@gmail.com>
2025-01-23 02:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let num= prompt("Enter a valid year");
+function daysofAYear(num){
+	if ((num % 4 === 0 && num % 100 !== 0) || num % 400 === 0){
+		return "366"
+	} else {
+		return "365"
+	}
+}
+alert(daysofAYear(num));
